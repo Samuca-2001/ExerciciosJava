4a43cb8906ad100b9209191e465448e547f831b1
Samuel da Rocha Santana <rochas563@gmail.com>
2025-11-10 14:46:15 -0300

    Adicionado as estruturas repetitivas

--- estrutura_repetitiva/while/Exercicio_1.java
+++ estrutura_repetitiva/while/Exercicio_1.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 public class Exercicio_1 {
     public static void main(String[] args) {
         
-    Scanner ler = new Scanner(System.in);
+        Scanner ler = new Scanner(System.in);
 
-    int senha;
+        int senha = 0;
 
-    System.out.println("Digite a senha: ");
-    senha = ler.nextInt();
-
-    while (senha != 2002) {
-        System.out.println("Senha inválida!");
         System.out.println("Digite a senha: ");
-    senha = ler.nextInt();
-    }
+        senha = ler.nextInt();
 
-    System.out.println("Acesso Permitido");
-    ler.close();
+        while (senha != 2002) {
+            System.out.println("Senha inválida!");
+            
+            System.out.println("Tente novamente: ");
+            senha = ler.nextInt(); 
+        }
+        
+        System.out.println("Acesso permitido!");
+        ler.close();
     }
 }
